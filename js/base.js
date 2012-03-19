286efd36d10e66cd7f698752418a05dd4f2423e8
liuyunclouder <liuyunclouder@gmail.com>
2012-03-19 23:35:54 +0800

    finish nav

--- js/base.js
+++ js/base.js
@@ -1,11 +1,21 @@
 (function(){
 	
-	
+	function nav_hover(e){
+		var $target = $(this);
+		
+		$target.toggleClass('hover');
+	};
 	
 	
 	$(function(){
 		
-		
+		$('#J_Nav li').hover(
+			function(){
+				$(this).addClass('hover');
+			},function(){
+				$(this).removeClass('hover');
+			}
+		);
 		
 	});
 	
