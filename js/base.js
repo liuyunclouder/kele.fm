59afc69ee25a5e5a0cc46da401489ce909b31625
liuyunclouder <liuyunclouder@gmail.com>
2012-03-20 23:27:36 +0800

    finish index and itemlist

--- js/base.js
+++ js/base.js
@@ -1,19 +1,36 @@
 (function(){
 	
-	function nav_hover(e){
-		var $target = $(this);
-		
-		$target.toggleClass('hover');
-	};
-	
+
 	
 	$(function(){
 		
+		//导航
 		$('#J_Nav li').hover(
 			function(){
 				$(this).addClass('hover');
 			},function(){
 				$(this).removeClass('hover');
+			}
+		);
+		
+		//瀑布流
+		$('#J_Pool').masonry({
+			itemSelector: '.item'
+		});
+		$('.item-link').hover(
+			function(){
+				$('h2, p',this).addClass('link-color');
+			},function(){
+				$('h2, p',this).removeClass('link-color');
+			}
+		);
+		
+		//漂浮子导航
+		$('#J_Fly_nav a').hover(
+			function(){
+				$(this).next().andSelf().addClass('c-bdt');
+			},function(){
+				$(this).next().andSelf().removeClass('c-bdt');
 			}
 		);
 		
